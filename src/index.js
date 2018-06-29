0511a1b010ae67034c221a60f2c8f6fef3934b4d
Dmitriy Ryajov <dryajov@gmail.com>
2018-06-28 20:15:17 -0600

    feat: webrtc circuit

--- src/index.js
+++ src/index.js
@@ -75,8 +75,8 @@ const Reliable = or(
 )
 
 const WebRTCCircuit = or(
-  and(Reliable, base('p2p-webrtc-circuit'), base('ipfs')),
-  and(base('p2p-webrtc-circuit'), base('ipfs'))
+  and(Reliable, base('ipfs'), base('p2p-webrtc-circuit')),
+  and(base('ipfs'), base('p2p-webrtc-circuit'))
 )
 
 let _IPFS = or(
