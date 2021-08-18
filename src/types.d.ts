d919e52201fe0bb8bbd8bce4de8ea5ad8bf84fe8
Vasco Santos <vasco.santos@moxy.studio>
2021-08-18 12:20:03 +0200

    chore: update uint8arrays (#87)

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,11 +1,13 @@
 import { Multiaddr } from 'multiaddr'
 
+// eslint-disable-next-line etc/prefer-interface
 export type MatchesFunction = (a: string | Uint8Array | Multiaddr) => boolean
+// eslint-disable-next-line etc/prefer-interface
 export type PartialMatchesFunction = (protos: string[]) => boolean | string[] | null
 
 export interface Mafmt {
   toString: () => string
-  input?: (Mafmt | (() => Mafmt))[]
+  input?: Array<(Mafmt | (() => Mafmt))>
   matches: MatchesFunction
   partialMatch: PartialMatchesFunction
 }
